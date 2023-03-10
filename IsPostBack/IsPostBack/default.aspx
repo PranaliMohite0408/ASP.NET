ae4eb6d514d98ba15a887529be52afa544347f2a
PranaliMohite0408 <pranalimohite482000@gmail.com>
2023-03-10 15:49:10 +0530

    ASP.NET IsPOstBack Property

---
+++ IsPostBack/IsPostBack/default.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="IsPostBack._default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:ListBox ID="ListBox1" runat="server"></asp:ListBox>
+            <br />
+            <br />
+            <br />
+            <asp:Button ID="btn_Click" runat="server" Text="Click" OnClick="Button1_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ IsPostBack/IsPostBack/default.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace IsPostBack
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           if(!IsPostBack)
+            {
+                AddListBoxItems();
+                Response.Write("IsPostBack Property is True");
+            }
+           else
+            {
+                Response.Write("IsPostBack Property is False");
+            }
+        }
+
+        void AddListBoxItems()
+        {
+            ListBox1.Items.Add("Items 1");
+            ListBox1.Items.Add("Items 2");
+            ListBox1.Items.Add("Items 3");
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
