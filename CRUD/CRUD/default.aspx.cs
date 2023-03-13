b04367321fc594f31b19ad5c3cf234cd71565ffe
PranaliMohite0408 <pranalimohite482000@gmail.com>
2023-03-13 10:40:26 +0530

    CRUD Application with GridView in ASP.NET

---
+++ CRUD/CRUD/default.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CRUD
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void LinkButton1_Click(object sender, EventArgs e)
+        {
+            SqlDataSource1.InsertParameters["Name"].DefaultValue = ((TextBox)GridView1.FooterRow.FindControl("FooterName")).Text;
+            SqlDataSource1.InsertParameters["Gender"].DefaultValue = ((DropDownList)GridView1.FooterRow.FindControl("FooterDDLGender")).SelectedValue;
+            SqlDataSource1.InsertParameters["Class"].DefaultValue = ((TextBox)GridView1.FooterRow.FindControl("FooterClass")).Text;
+
+            int a = SqlDataSource1.Insert();
+            if(a > 0)
+            {
+                Page.ClientScript.RegisterStartupScript(this.GetType(),"Scripts", "<script>alert('Record Save Sucessfully !!')</script>");
+            }
+            else
+            {
+                Page.ClientScript.RegisterStartupScript(this.GetType(), "Scripts", "<script>alert('Failed  !!')</script>");
+            }
+        }
+    }
+}
